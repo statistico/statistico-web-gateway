d8e0606e86719170952c0a637cd6f7f836d58659
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-06-30 22:15:53 +0100

    Add team handler and tests

--- internal/app/rest/team.go
+++ internal/app/rest/team.go
@@ -1,1 +1,43 @@
 package rest
+
+import (
+	"errors"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/statistico/statistico-web-gateway/internal/app/composer"
+	e "github.com/statistico/statistico-web-gateway/internal/app/errors"
+	"net/http"
+	"strconv"
+)
+
+type TeamHandler struct {
+	composer composer.TeamComposer
+}
+
+func (t *TeamHandler) TeamById(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
+	param := ps.ByName("id")
+	id, err := strconv.Atoi(param)
+
+	if err != nil {
+		failResponse(w, http.StatusNotFound, errors.New(fmt.Sprintf("team with id '%s' does not exist", param)))
+		return
+	}
+
+	team, err := t.composer.TeamById(uint64(id))
+
+	if err != nil {
+		if err == e.ErrorNotFound {
+			failResponse(w, http.StatusNotFound, err)
+			return
+		}
+
+		errorResponse(w, http.StatusBadGateway, errors.New("bad gateway"))
+		return
+	}
+
+	successResponse(w, http.StatusOK, team)
+}
+
+func NewTeamHandler(c composer.TeamComposer) *TeamHandler {
+	return &TeamHandler{c}
+}
--- internal/app/team.go
+++ internal/app/team.go
@@ -3,10 +3,10 @@ package app
 type Team struct {
 	ID           uint64  `json:"id"`
 	Name         string  `json:"name"`
-	ShortCode    *string `json:"short_code"`
-	CountryID    uint64  `json:"country_id"`
-	VenueID      uint64  `json:"venue_id"`
-	NationalTeam bool    `json:"national_team"`
+	ShortCode    *string `json:"shortCode"`
+	CountryID    uint64  `json:"countryId"`
+	VenueID      uint64  `json:"venueId"`
+	NationalTeam bool    `json:"nationalTeam"`
 	Founded      *uint64 `json:"founded"`
 	Logo         *string `json:"logo"`
 }
