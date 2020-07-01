141baa22105e15becf2a04a94265b159660acf8d
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-07-01 18:07:28 +0100

    Add access control headers

--- internal/app/rest/response.go
+++ internal/app/rest/response.go
@@ -19,6 +19,8 @@ func jsonResponse(w http.ResponseWriter, status int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	w.WriteHeader(status)
 	_, _ = w.Write(response)
 }
