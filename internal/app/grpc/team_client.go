4ed36cb84a0f6ab05dac7e5e20ede6f8aa295119
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-07-31 13:40:23 +0100

    Add team client method

--- internal/app/grpc/team_client.go
+++ internal/app/grpc/team_client.go
@@ -8,10 +8,12 @@ import (
 	"github.com/statistico/statistico-web-gateway/internal/app/grpc/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
 )
 
 type TeamClient interface {
 	TeamById(ctx context.Context, req *proto.TeamRequest) (*app.Team, error)
+	TeamsBySeasonId(ctx context.Context, seasonId uint64) ([]*app.Team, error)
 }
 
 type teamClient struct {
@@ -41,6 +43,46 @@ func (t teamClient) TeamById(ctx context.Context, req *proto.TeamRequest) (*app.
 	team := convertTeam(response)
 
 	return &team, nil
+}
+
+func (t teamClient) TeamsBySeasonId(ctx context.Context, seasonId uint64) ([]*app.Team, error) {
+	teams := []*app.Team{}
+
+	req := proto.SeasonTeamsRequest{SeasonId: seasonId}
+
+	stream, err := t.client.GetTeamsBySeasonId(ctx, &req)
+
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.Internal:
+				t.logError(err)
+				return teams, errors.ErrorInternalServerError
+			default:
+				t.logError(err)
+				return teams, errors.ErrorBadGateway
+			}
+		}
+	}
+
+	for {
+		team, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			t.logError(err)
+			return teams, errors.ErrorInternalServerError
+		}
+
+		te := convertTeam(team)
+
+		teams = append(teams, &te)
+	}
+
+	return teams, nil
 }
 
 func (t teamClient) logError(err error) {
